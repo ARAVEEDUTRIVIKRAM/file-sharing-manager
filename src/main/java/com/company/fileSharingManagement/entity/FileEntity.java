8bdd82100e0152b8e4341a8917dea1e715783dfc
ARAVEEDUTRIVIKRAM <araveedutrivikram@gmail.com>
2025-07-03 14:30:35 +0530

    Updated FileModel, FileEntity, and pom.xml

--- src/main/java/com/company/fileSharingManagement/entity/FileEntity.java
+++ src/main/java/com/company/fileSharingManagement/entity/FileEntity.java
@@ -1,27 +1,74 @@
 package com.company.fileSharingManagement.entity;
 
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
-
-import jakarta.persistence.*;
-import lombok.Data;
-
 @Entity
-@Data
-public class FileEntity{
+@Table(name = "files")
+public class FileEntity {
 
     @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String fileName;
+
     private String uploadedBy;
+
     private LocalDateTime uploadTime;
+
     private LocalDateTime expiryTime;
-    
 
     @Lob
-    @Column(name = "file_data", columnDefinition="LONGBLOB")
     private byte[] fileData;
 
-} 
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getUploadedBy() {
+        return uploadedBy;
+    }
+
+    public void setUploadedBy(String uploadedBy) {
+        this.uploadedBy = uploadedBy;
+    }
+
+    public LocalDateTime getUploadTime() {
+        return uploadTime;
+    }
+
+    public void setUploadTime(LocalDateTime uploadTime) {
+        this.uploadTime = uploadTime;
+    }
+
+    public LocalDateTime getExpiryTime() {
+        return expiryTime;
+    }
+
+    public void setExpiryTime(LocalDateTime expiryTime) {
+        this.expiryTime = expiryTime;
+    }
+
+    public byte[] getFileData() {
+        return fileData;
+    }
+
+    public void setFileData(byte[] fileData) {
+        this.fileData = fileData;
+    }
+}
--- src/main/java/com/company/fileSharingManagement/model/FileModel.java
+++ src/main/java/com/company/fileSharingManagement/model/FileModel.java
@@ -2,15 +2,47 @@ package com.company.fileSharingManagement.model;
 
 import java.time.LocalDateTime;
 
-import lombok.Data;
-
-
-@Data
 public class FileModel {
-    private int id;
+
     private String fileName;
+
     private String uploadedBy;
-    private LocalDateTime uploadTime;
+
     private LocalDateTime expiryTime;
+
     private byte[] fileData;
+
+    // Getters and Setters 
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getUploadedBy() {
+        return uploadedBy;
+    }
+
+    public void setUploadedBy(String uploadedBy) {
+        this.uploadedBy = uploadedBy;
+    }
+
+    public LocalDateTime getExpiryTime() {
+        return expiryTime;
+    }
+
+    public void setExpiryTime(LocalDateTime expiryTime) {
+        this.expiryTime = expiryTime;
+    }
+
+    public byte[] getFileData() {
+        return fileData;
+    }
+
+    public void setFileData(byte[] fileData) {
+        this.fileData = fileData;
+    }
 }
